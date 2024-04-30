f0b3948a5735dc8b6d312fc35f415703118cc30e
Oxshady <shadymahmod55@gmail.com>
2024-04-30 23:11:01 +0300

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,4 @@
 #include "sort.h"
-/**
- * _swap - swap between to elements of array
- * @arr: address of first element of the array
- * @ind_one: index 1
- * @ind_two: index2
- */
-void _swap(int *arr, int ind_one, int ind_two)
-{
-	int aux = arr[ind_one];
-
-	arr[ind_one] = arr[ind_two];
-	arr[ind_two] = aux;
-}
 
 /**
  *  bubble_sort - sorts an array of integers in ascending order
